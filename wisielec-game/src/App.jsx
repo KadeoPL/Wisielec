e30c75bc6037b9d011095a3f52402c2eb5f80f7e
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-05-14 21:43:55 +0200

    add useState

--- wisielec-game/src/App.jsx
+++ wisielec-game/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 import Letter from "./components/letter"
 
 function App() {
+  const typedLetter = useState(letters, setLetter);
 
+  function handelAddLetter (){
+    setLetter(prevLetter => {
+      
+    })
+  }
   return (
     <>
       <Letter letter="P"></Letter>
