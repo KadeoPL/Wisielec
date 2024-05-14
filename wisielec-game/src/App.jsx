53973e02bd13ec3ec36d4a9f829d50d492b083ce
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-05-14 21:47:34 +0200

    add onClick

--- wisielec-game/src/App.jsx
+++ wisielec-game/src/App.jsx
@@ -4,14 +4,10 @@ import Letter from "./components/letter"
 function App() {
   const typedLetter = useState(letters, setLetter);
 
-  function handelAddLetter (){
-    setLetter(prevLetter => {
-      
-    })
-  }
   return (
     <>
-      <Letter letter="P"></Letter>
+      <Letter letter={letters}></Letter>
+      <button onClick={setLetter}>Ok</button>
     </>
   )
 }
--- wisielec-game/src/components/letter.jsx
+++ wisielec-game/src/components/letter.jsx
@@ -2,7 +2,6 @@ export default function Letter({letter}) {
     
     return (
         <> 
-            <h2>Typed letters:</h2>
             <h1>{letter}</h1>
         </>
     )
