3b8254ba58ce31e21df521e12b9b219ea5946b87
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-05-17 17:59:19 +0200

    add hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import TypedLetterList from "./components/TypedLettersList";
 import TypeLetterForm from "./components/TypeLetterForm";
-import WordSelector from "./components/WordSelector";
+import { getRandomWord } from "./components/WordSelector";
 
 function App() {
   const [letters, setLetters] = useState([]);
+  const [randomWord, setRandomWord] = useState('');
+
+  useEffect(() => {
+    const newRandomWord = getRandomWord();
+    setRandomWord(newRandomWord);
+  }, [])
 
   function handleAddLetter(letter) {
       setLetters(prevLetters => [...prevLetters, letter]);
   }
 
+  function handleGenerateWord(){
+    const newRandomWord = getRandomWord();
+    setRandomWord(newRandomWord);
+  }
+
   return (
     <>
-      <WordSelector />
+      <div>
+        <p>{randomWord.word}</p>
+        <p>{randomWord.category}</p>
+      </div>
       <TypeLetterForm onAddLetter={handleAddLetter}/>
       <TypedLetterList letters={letters} />
+      <button onClick={handleGenerateWord}>Generate new word</button>
     </>
   )
 }
--- src/components/WordSelector.jsx
+++ src/components/WordSelector.jsx
@@ -1,17 +1,13 @@
-export default function WordSelector(){
-    
-    const wordsArray = [
+    export const wordsArray = [
         {word: 'Computer', category: 'Information technology'},
-        {word: 'Phone', category: 'Phone technology'}
+        {word: 'Phone', category: 'Phone technology'},
+        {word: 'Keyboard', category: 'Information technology'},
+        {word: 'Mouse', category: 'Information technology'}
     ];
 
     const randomIndex = Math.floor(Math.random() * wordsArray.length);
-    const randomWord = wordsArray[randomIndex];
 
-    return (
-        <div>
-            <p>{randomWord.word}</p>
-            <p>{randomWord.category}</p>
-        </div>
-    )
-}
+    export function getRandomWord(){
+        const randomIndex = Math.floor(Math.random() * wordsArray.length);
+        return wordsArray[randomIndex];
+    }
