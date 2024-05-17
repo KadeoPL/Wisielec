92dd2fccb804f15ea4e6d404ed43a1e748a0413e
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-05-17 18:20:43 +0200

    add validation to form

--- src/components/TypeLetterForm.jsx
+++ src/components/TypeLetterForm.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 
 export default function TypeLetterForm ({onAddLetter}) {
     const [enteredLetter, setEnteredLetter] = useState('')
+    const [error, setError] = useState('')
     
     function handleSubmit (event){
         event.preventDefault();
-
-        onAddLetter(enteredLetter);
-        setEnteredLetter(''); 
-    }
+        
+        if (/^[a-zA-Z]$/.test(enteredLetter)) {
+            onAddLetter(enteredLetter);
+            setEnteredLetter('');
+            setError('');
+          } else if(enteredLetter.length === 0){
+            setError('The field cannot be empty');
+          } else {
+            setError('The character you enter must be a letter');
+          }
+        }
     
     return (
         <form onSubmit={handleSubmit}>
@@ -18,8 +26,10 @@ export default function TypeLetterForm ({onAddLetter}) {
                     type="text"
                     value={enteredLetter}
                     onChange={(event) => setEnteredLetter(event.target.value)}
+                    maxLength={1}
                 />
             </p>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
             <p>
                 <button type="submit">OK</button>
             </p>
